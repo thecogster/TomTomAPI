bbcec162a262959d41032c759416dc465f2b3463
thecogster <cianograd@gmail.com>
2022-03-04 15:06:05 +0000

    Adding Data collection function

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ from turtle import pd
 
 import pandas as pd
 import numpy as np
+import requests
+import json
 
+def datacollection():
+    url = 'https://api.tomtom.com/search/2/poiSearch/Chinese%20Restaurant.json?key=MaG1sGHK8IEzPNsP2CsGt31gTcMGJx1E&categoryset=7315&countrySet=NL&limit=50'
+    r = requests.get(url)
+    parsed = json.loads(r.content)
+    data = parsed['results']
+    return data
